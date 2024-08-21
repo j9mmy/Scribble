2791a266e3c719097a34be500a1ef4647528a8a2
j9mmy <jimmy.tukiran@outlook.com>
2024-08-21 20:06:14 +0200

    feat: login page
    the login page correctly communicates with a proper back-end. users will be able to login with a created account, and receive UI/UX feedback based on their actions.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,29 +1,61 @@
 import { useState } from 'react'
 import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Link } from 'react-router-dom';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Loader2, AlertCircle } from 'lucide-react';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false)
+  const [loginStatus, setLoginStatus] = useState(0)
+  const navigate = useNavigate()
 
-  const loginSubmit = async () => {
-    console.log('Login attempt \nEmail:', email, '\nPassword:', password);
+  const formSchema = z.object({
+    email: z.string().email("Valid email is required"),
+    password : z.string().min(1, "Password is required")
+  })
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true)
+    setLoginStatus(0)
 
     axios.post('api/user/login', {
-      email: email,
-      password: password
+      email: values.email,
+      password: values.password,
     })
     .then(response => {
       console.log(response.data);
-      logoutSubmit();
+      navigate('/dashboard')
+      logoutSubmit(); // Temporary ofcourse
     })
     .catch(error => {
       console.error(error);
-    });
+      setLoginStatus(error.response.status)
+    })
+    .finally(() => setIsLoading(false));
   }
+
 
   const logoutSubmit = async () => {
     console.log('Logging current user out...')
@@ -31,6 +63,7 @@ function Login() {
     axios.post('api/user/logout')
     .then(response => {
       console.log(response.data);
+      navigate('/')
     })
     .catch(error => {
       console.error(error);
@@ -41,31 +74,77 @@ function Login() {
     <>
       <div className='flex flex-col items-center justify-center min-h-screen'>
         <div className='space-y-6 max-w-sm w-full mx-4'>
-            <header>
-                <h1>Scribble</h1>
-                <p className="text-sm text-slate-500">Welcome back! Please login to start Scribbling.</p>
-            </header>
-            <div className='space-y-3'>
-                <div className='space-y-1'>
-                    <Label htmlFor="email">Email</Label>
-                    <Input onChange={e => setEmail(e.target.value)} placeholder="Email"/>
-                </div>
-                <div className='space-y-1'>
-                    <Label htmlFor="password">Password</Label>
-                    <Input onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
-                </div>
-            </div>
-            <footer className='flex flex-col space-y-3 justify-between'>
-                <Button onClick={loginSubmit}>Login</Button>
-                <Button variant='outline' asChild>
+
+          <header>
+              <h1>Scribble</h1>
+              <p className="text-sm text-muted-foreground">Welcome back! Please login to start Scribbling.</p>
+          </header>
+
+          <Form {...form}>
+            <form className='flex flex-col gap-8' onSubmit={form.handleSubmit(onSubmit)}>
+
+              <div className='flex flex-col gap-4'>
+                <FormField
+                  control={form.control}
+                  name='email'
+                  render={({field}) => (
+                    <FormItem className='flex flex-col gap-1'>
+                      <Label>Email</Label>
+                      <FormControl>
+                        <Input type='email' placeholder='Email' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({field}) => (
+                    <FormItem className='flex flex-col gap-1'>
+                      <Label>Password</Label>
+                      <FormControl>
+                        <Input type='password' placeholder='Password' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className='flex flex-col gap-3 justify-between'>
+                <Button type='submit' disabled={isLoading} onClick={() => onSubmit}>
+                  {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                  Login
+                </Button>
+                <Button type='button' variant='outline' asChild>
                   <Link to='/register'>Create a new account</Link>
                 </Button>
                 <div className='flex justify-center items-center h-10'>
-                  <Button variant="link" className='w-fit h-fit p-0'>
+                  <Button type='button' variant="link" className='w-fit h-fit p-0'>
                     <Link to='/password'>Password forgotten?</Link>
                   </Button>
                 </div>
-            </footer>
+              </div>
+              
+              {loginStatus >= 400 && loginStatus < 500 && (
+                <Alert variant={'destructive'}>
+                  <AlertCircle className='h-4 w-4' />
+                  <AlertTitle>Login failed</AlertTitle>
+                  <AlertDescription>
+                    {loginStatus == 404 && (
+                      'No user found with this email'
+                    )}
+                    {loginStatus == 401 && (
+                      'Incorrect email and/or password combination'
+                    )}
+                  </AlertDescription>
+                </Alert>
+              )}
+
+            </form>
+          </Form>
         </div>
       </div>
     </>
